0a2c995a53c5e9a5ba7d077bcf23fe369d5f297c
DESKTOP-FJ1EJER\quocn <quocngu4616@gmail.com>
2021-04-01 04:03:54 +0700

    responsive page

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,18 @@ const handleClickOpenSidebar=()=> {
 const handleClickCloseSidebar=()=> {
     document.querySelector('.sidebar').classList.remove("opensidebar")
 }
+
+var modal = document.getElementById("myModal");
+function clickToShowModal(obj){
+    var modalImg = document.getElementById("img01");
+
+    var parentObj=obj.parentNode
+    console.log(parentObj)
+    image=parentObj.children[0]
+    modalImg.src=image.src
+    modal.style.display = "block";
+}
+function closeModal(){
+    // var span = document.getElementsByClassName("close")[0];
+modal.style.display="none"
+}
